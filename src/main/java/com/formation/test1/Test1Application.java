b77eeb2ba47365ba12d2dcef337e5a098942a6f3
GBODJO DOGBEDA <dgbodjo@bceao.int>
2024-05-27 15:39:22 +0000

    First commit

---
+++ src/main/java/com/formation/test1/Test1Application.java
@@ -0,0 +1,21 @@
+package com.formation.test1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Test1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Test1Application.class, args);
+	}
+
+	@GetMapping("hello")
+	public String hello() {
+		return "<h2>Hello from Tomcat.</h2>";
+	}
+
+}
